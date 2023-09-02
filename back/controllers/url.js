b12fcfbfb631be6fa14f9ba9e17123d4dc439fe4
sayamsharma <ssayam200@gmail.com>
2023-09-02 21:05:06 +0530

    commit

--- back/controllers/url.js
+++ back/controllers/url.js
@@ -4,6 +4,7 @@ const URL = require('../module/url'); // Assuming you have a URL model defined
 async function handleGenerateShortUrl(req, res) {
     try {
         const shortUrl = shortid.generate(); // Use shortid.generate() instead of shortid()
+        const { Url } = req.body;
         const urlRecord = await URL.create({
             shortId: shortUrl,
             redirectUrl: Url,
@@ -14,7 +15,7 @@ async function handleGenerateShortUrl(req, res) {
             id: urlRecord.shortId
         });
     } catch (error) {
-        return res.status(500).json({ 'Error': 'Internal server error.' });
+        return res.status(400).json({ 'Error': 'Url is requird.' });
     }
 }
 
